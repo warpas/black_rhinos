3cbe971dd3475a8e99e651090bfa17052c02ef32
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2020-08-13 22:07:08 +0200

    Add an example of working with JS code based on Three.js

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def main
   end
+
+  def three_example
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#main"
   resources :contacts
+  get '/three_js_example', to: 'pages#three_example'
 end
