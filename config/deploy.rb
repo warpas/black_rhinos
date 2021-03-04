ee98cfa977b300f6e231374273499ea396825d5d
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2021-03-04 22:33:29 +0100

    Update app name in deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "black_rhinos"
+set :repo_url, "git@github.com:warpas/black_rhinos.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
